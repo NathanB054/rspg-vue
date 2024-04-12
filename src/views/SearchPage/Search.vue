6be7bab4ec36d1a2bda7a6cfbe0f53e799e3b61a
NathanB054 <6431501054@lamduan.mfu.ac.th>
2024-04-12 07:40:03 +0700

    Update Search.vue

--- src/views/SearchPage/Search.vue
+++ src/views/SearchPage/Search.vue
@@ -1,10 +1,37 @@
 <template>
-    <div class="center">
+  <div class="center">
+    <div class="content">
       <div v-if="isLoading" class="spinner-container">
         <div class="spinner"></div>
       </div>
-  
-      <table v-else class="table-clickable">
+      <table v-else>
+        <caption>ผลลัพธ์ทั้งหมด {{ plants.length }}</caption>
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Avatar</th>
+            <th scope="col">City</th>
+          </tr>
+        </thead>
+        <tbody v-for="plant in plants" @click="clickList(plant.id)">
+          <tr class="info">
+            <td data-label="ID">
+              {{ plant.id }}
+            </td>
+            <td data-label="Name">
+              {{ plant.plant_name }}
+            </td>
+            <td data-label="Avatar">
+              {{ plant.avatar }}
+            </td>
+            <td data-label="City">
+              {{ plant.City }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <!-- <table  class="table-clickable">
         <thead>
             <h1>ผลลัพธ์ทั้งหมด {{ plants.length }}</h1>
           <tr>
@@ -39,215 +66,173 @@
             </td>
           </tr>
         </tbody>
-      </table>
+      </table> -->
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        plants: [],
-        isLoading: true, // Initially set loading to true
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      plants: [],
+      isLoading: true, // Initially set loading to true
+    };
+  },
+  mounted() {
+    fetch('https://65fb5ab714650eb21009db19.mockapi.io/plant')
+      .then(res => res.json())
+      .then(data => {
+        this.plants = data;
+        this.isLoading = false; // Set loading to false after data is fetched
+      })
+      .catch(err => console.error(err));
+  },
+  methods: {
+    clickList(id) {
+      this.$router.push(`/searchDetail/${id}`);
     },
-    mounted() {
-      fetch('https://65fb5ab714650eb21009db19.mockapi.io/plant')
-        .then(res => res.json())
-        .then(data => {
-          this.plants = data;
-          this.isLoading = false; // Set loading to false after data is fetched
-        })
-        .catch(err => console.error(err));
-    },
-  };
-  </script>
+  }
+};
+</script>
 
 <style scoped>
 .center {
-    margin: auto;
-    width: 40%;
-    padding: 10px;
-    overflow-y: auto;
-    height: 400px;
-    margin-top: 5%;
-    margin-bottom: 5%;
+  margin: auto;
+  width: 60%;
+  overflow-y: auto;
+  height: 30em;
+  margin-top: 5%;
+  margin-bottom: 5%;
+ border-radius: 50px;
 }
+
+.content {
+  background-color: #86869b;
+
+}
+
 
 table {
-    border-spacing: 1;
-    border-collapse: collapse;
-    background: #efdcef;
-    border-radius: 6px;
-    overflow: hidden;
-    max-width: 800px;
-    width: 100%;
-    margin: 0 auto;
-    position: relative;
-    
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  table-layout: fixed;
 }
 
-table * {
-    position: relative;
+table caption {
+  font-size: 1.5em;
+  margin: .5em 0 .75em;
 }
 
-
-
-table thead tr {
-    height: 60px;
-    background: #efdcef;
-    font-size: 16px;
+table tr {
+  background-color: #e1aef5;
+  padding: .35em;
 }
 
-table tbody tr {
-    height: 48px;
-    border-bottom: 1px solid #f1e6f0;
+table .info:hover {
+  cursor: pointer;
+  background-color: #e1aef54b;
 }
 
-table tbody tr:last-child {
-    border: 0;
+table th,
+table td {
+  padding: .625em;
+  text-align: center;
 }
 
-table td,
 table th {
-    text-align: left;
+  font-size: .85em;
+  letter-spacing: .1em;
+  text-transform: uppercase;
 }
 
-table td.l,
-table th.l {
-    text-align: right;
-}
+@media screen and (max-width: 600px) {
+  table {
+    border: 0;
+  }
 
-table td.c,
-table th.c {
-    text-align: center;
-}
+  table caption {
+    font-size: 1.3em;
+  }
 
-table td.r,
-table th.r {
-    text-align: center;
-}
-.a{
- margin-left: -1%;
-}
+  table thead {
+    border: none;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+  }
 
-@media screen and (max-width: 35.5em) {
-    table {
-        display: block;
-    }
-
-    table>*,
-    table tr,
-    table td,
-    table th {
-        display: block;
-    }
-
-    table thead {
-        display: none;
-    }
-
-    table tbody tr {
-        height: auto;
-       
-    }
-
-    table tbody tr td {
-        padding-left: 45%;
-        margin-bottom: 12px;
-    }
-
-    table tbody tr td:last-child {
-        margin-bottom: 0;
-    }
-
-    table tbody tr td:before {
-        position: absolute;
-        font-weight: 700;
-        width: 40%;
-        left: 10px;
-        top: 0;
-    }
-
-    table tbody tr td:nth-child(1):before {
-        content: "ID";
-    }
-
-    table tbody tr td:nth-child(2):before {
-        content: "Name";
-    }
-
-    table tbody tr td:nth-child(3):before {
-        content: "Avatar";
-    }
-
-    table tbody tr td:nth-child(4):before {
-        content: "City";
-    }
-
-}
-
-body {
-    background: #7c8375;
-    font: 400 14px 'Calibri', 'Arial';
-    padding: 20px;
-}
-
-
-
-/* TABLE STYLES
-–––––––––––––––––––––––––––––––––––––––––––––––––– */
-.table-clickable {
-    text-align: left;
-    border-collapse: collapse;
-    margin: 20px auto;
-}
-
-.table-clickable th,
-.table-clickable tbody .a {
-    padding: 10px 10px;
-}
-
-
-.table-clickable tbody .a {
+  table tr {
+    border-bottom: 3px solid #ddd;
     display: block;
-    text-decoration: none;
-    transition: all 0.25s ease-out;
+    margin-bottom: .625em;
+  }
+
+  table td {
+    border-bottom: 1px solid #ddd;
+    display: block;
+    font-size: .8em;
+    text-align: right;
+  }
+
+  table td::before {
+    /*
+    * aria-label has no advantage, it won't be read inside a table
+    content: attr(aria-label);
+    */
+    content: attr(data-label);
+    float: left;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  table td:last-child {
+    border-bottom: 0;
+  }
 }
 
-.table-clickable tbody tr:hover .a {
-    color: white;
-    background: #85508a;
-}
+
+
 
 
 .spinner {
-   width: 56px;
-   height: 56px;
-   --c: radial-gradient(farthest-side,#474bff 92%,#0000);
-   background: var(--c) 50% 0,
-          var(--c) 50% 100%,
-          var(--c) 100% 50%,
-          var(--c) 0    50%;
-   background-size: 13.4px 13.4px;
-   background-repeat: no-repeat;
-   animation: spinner-kh173p 1s infinite;
+  width: 56px;
+  height: 56px;
+  --c: radial-gradient(farthest-side, #474bff 92%, #0000);
+  background: var(--c) 50% 0,
+    var(--c) 50% 100%,
+    var(--c) 100% 50%,
+    var(--c) 0 50%;
+  background-size: 13.4px 13.4px;
+  background-repeat: no-repeat;
+  animation: spinner-kh173p 1s infinite;
 }
 
 @keyframes spinner-kh173p {
-   to {
-      transform: rotate(.5turn);
-   }
-}
-.spinner-container {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    /* background-color: rgba(0, 0, 0, 0.5); Semi-transparent overlay */
-    display: flex; /* Allow centering of spinner */
-    justify-content: center; /* Horizontally center spinner */
-    align-items: center; /* Vertically center spinner */
+  to {
+    transform: rotate(.5turn);
   }
+}
+
+.spinner-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  /* background-color: rgba(0, 0, 0, 0.5); Semi-transparent overlay */
+  display: flex;
+  /* Allow centering of spinner */
+  justify-content: center;
+  /* Horizontally center spinner */
+  align-items: center;
+  /* Vertically center spinner */
+}
 </style>
